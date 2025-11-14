e194607dd8a4279885a2cb02f80f58cc820fb6ed
Shivahari Rukesh <478gnujnoej@gmail.com>
2025-11-14 22:16:47 +0545

    experience card animation

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,30 +1,35 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import ExperienceCard from "../ExperienceCard/ExperienceCard";
 import gsap from "gsap";
 import type { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ExperienceDetails } from "./ExperienceDetails";
 
+
 const Experience = () => {
-    const cardRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
 
         const triggers: ScrollTrigger[] = []
-        const children = cardRef.current?.children;
-        if (!children) return;
+        const experienceTapeChildren = document.getElementsByClassName('experience-tape')
+        const experienceTVChildren = document.getElementsByClassName('experience-tv')
 
-        Array.from(children).forEach((ele, index) => {
+        // const experienceTapeChildren = cardRef.current?.experienceTapeChildren;
+        console.log(experienceTapeChildren)
+        if (!experienceTapeChildren) return;
+        gsap.set(experienceTVChildren, { opacity: 0 })
 
+
+        Array.from(experienceTapeChildren).forEach((ele, index) => {
             gsap.set(ele, { opacity: 0 })
 
             const trigger = gsap.to(ele, {
-                x: index % 2 === 0 ? -300 : 300,
+                x: index % 2 === 0 ? -200 : 200,
                 opacity: 1,
                 duration: 0.8,
                 scrollTrigger: {
                     trigger: ele,
                     start: "top center",
-                    toggleActions: "play reverse play reverse"
+                    // toggleActions: "play reverse play reverse"
 
                 }
             });
@@ -32,20 +37,38 @@ const Experience = () => {
             triggers.push(trigger.scrollTrigger as ScrollTrigger)
         });
 
+
         return () => {
             triggers.forEach((t) => t.kill())
         }
     }, []);
 
+    const handlePlayTV = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        console.log(e.currentTarget.parentElement?.lastChild)
+        const triggeredCurrentDiv = e.currentTarget
+        const triggeredCurrentDivLastSibiling = e.currentTarget.parentElement?.lastChild as HTMLDivElement
+        gsap.to(triggeredCurrentDiv, {
+            x: -400,
+            opacity: 0,
+            duration: 0.8,
+        })
+
+        gsap.to(triggeredCurrentDivLastSibiling, {
+
+            x: -400,
+            opacity: 1,
+            duration: 1,
+        })
+    }
+
     return (
         <section id="experience" className="h-[200vh] w-full p-16 ">
             <div
-                ref={cardRef}
                 className="flex flex-col justify-around items-center h-full w-full"
             >
-                {ExperienceDetails.map((detail) => {
+                {ExperienceDetails.map((detail, index) => {
 
-                    return <ExperienceCard {...detail} />
+                    return <ExperienceCard key={index} {...detail} handlePlayTV={handlePlayTV} />
 
                 }
                 )
--- src/components/ExperienceCard/ExperienceCard.tsx
+++ src/components/ExperienceCard/ExperienceCard.tsx
@@ -1,7 +1,8 @@
 import type { ExperienceDetailType } from '../../types/experienceDetail'
 import './ExperienceCard.css'
 
-const Card = ({ date, companyName, location, position, backgroundImage }: ExperienceDetailType) => {
+type ExperienceCardProps = ExperienceDetailType & { handlePlayTV: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void }
+const ExperienceCard = ({ date, companyName, location, position, handlePlayTV }: ExperienceCardProps) => {
 
 
 
@@ -46,92 +47,188 @@ const Card = ({ date, companyName, location, position, backgroundImage }: Experi
         //     </div>
         // </div>
 
+        <div className='flex'>
 
-        <div className="card-box relative w-[600px] h-[380px]">
-            <div className="absolute inset-0 bg-gradient-to-b from-gray-900/95 to-black/95 rounded-md shadow-2xl border-2 border-gray-700">
 
-                <div className="absolute top-4 left-4 right-4 h-28 bg-gradient-to-b from-amber-50 to-amber-500 rounded-sm shadow-md border border-orange-200">
-                    {/* <img
+            <div className="experience-tape relative w-[600px] h-[380px] border-t-8 rounded-r-2xl border-amber-900"
+                onClick={(e: React.MouseEvent<HTMLDivElement, MouseEvent>) => handlePlayTV(e)}
+            >
+                <div className="absolute inset-0 bg-gradient-to-b from-gray-900/95 to-black/95 rounded-md shadow-2xl border-2 border-gray-700">
+
+                    <div className="absolute top-4 left-4 right-4 h-28 bg-gradient-to-b from-amber-50 to-amber-500 rounded-sm shadow-md border border-orange-200">
+                        {/* <img
                         src={backgroundImage}
                         alt={backgroundImage.slice(0, -4)}
                         className="w-full h-full object-cover  transition-transform duration-700 group-hover:scale-110"
                     /> */}
-                    <div className="p-2">
+                        <div className="p-2">
 
-                        <div className="text-3xl font-bold text-gray-800 mb-1">{companyName}</div>
-                        <div className="text-2xl text-gray-600">{location}</div>
+                            <div className="text-3xl font-bold text-gray-800 mb-1">{companyName}</div>
+                            <div className="text-2xl text-gray-600">{location}</div>
+                        </div>
                     </div>
-                </div>
 
-                <div className="absolute top-36 left-4 right-4 h-20 bg-gradient-to-r from-gray-600 via-gray-500 to-gray-600 rounded-sm shadow-inner">
-                    <div className="flex items-center justify-between px-8 h-full">
 
-                        <div className="relative">
-                            <div className="w-12 h-12 rounded-2xl bg-gradient-to-br from-gray-800 to-black border-2 border-gray-700 shadow-xl"></div>
-                            <div className="absolute inset-0 flex items-center justify-center">
-                                L
+
+                    <div className="absolute top-36 left-4 right-4 h-20 bg-gradient-to-r from-gray-600 via-gray-500 to-gray-600 rounded-sm shadow-inner">
+                        <div className="flex items-center justify-between px-8 h-full">
+
+                            <div className="relative">
+                                <div className="w-12 h-12 rounded-2xl bg-gradient-to-br from-gray-800 to-black border-2 border-gray-700 shadow-xl"></div>
+                                <div className="absolute inset-0 flex items-center justify-center">
+                                    L
+                                </div>
                             </div>
-                        </div>
 
-                        <div className="flex-1 mx-6 h-10 bg-black rounded-sm border border-gray-800 shadow-inner flex items-center justify-center">
-                            <div className="text-green-500 text-lg font-mono tracking-wider opacity-80">{position}</div>
-                        </div>
-
-                        <div className="relative">
-                            <div className="w-12 h-12 rounded-2xl bg-gradient-to-br from-gray-800 to-black border-2 border-gray-700 shadow-xl"></div>
-                            <div className="absolute inset-0 flex items-center justify-center">R
+                            <div className="flex-1 mx-6 h-10 bg-black rounded-sm border border-gray-800 shadow-inner flex items-center justify-center">
+                                <div className="text-green-500 text-lg font-mono tracking-wider opacity-80">{position}</div>
                             </div>
-                        </div>
-                    </div>
 
-                    <div className="absolute bottom-1 left-0 right-0 text-center">
-                        <div className="text-white text-xs font-bold tracking-wide">{date}</div>
-                    </div>
-                </div>
-
-                <div className="absolute top-60 left-12 right-12 flex items-center justify-between">
-
-                    <div className="relative">
-                        <div className="w-28 h-28 rounded-full bg-gradient-to-br from-gray-800 to-gray-900 border-4 border-gray-700 shadow-2xl flex items-center justify-center">
-                            <div className="w-20 h-20 rounded-full bg-gray-900 flex items-center justify-center">
-                                <div className="w-12 h-12 rounded-full bg-black border-2 border-gray-600 flex items-center justify-center">
-                                    <div className="w-6 h-6 rounded-full bg-gray-900"></div>
+                            <div className="relative">
+                                <div className="w-12 h-12 rounded-2xl bg-gradient-to-br from-gray-800 to-black border-2 border-gray-700 shadow-xl"></div>
+                                <div className="absolute inset-0 flex items-center justify-center">R
                                 </div>
                             </div>
                         </div>
 
-                    </div>
-
-                    <div className="flex-1 mx-6 h-16 relative">
-                        <div className="absolute inset-0 bg-gradient-to-b from-gray-900 to-black rounded-sm border-2 border-gray-800 overflow-hidden">
-                            <div className="absolute inset-0 flex items-center justify-center">
-                                <div className="w-full h-4 bg-gradient-to-r from-amber-950 via-amber-900 to-amber-950"></div>
-                            </div>
+                        <div className="absolute bottom-1 left-0 right-0 text-center">
+                            <div className="text-white text-xs font-bold tracking-wide">{date}</div>
                         </div>
                     </div>
 
-                    <div className="relative">
-                        <div className="w-28 h-28 rounded-full bg-gradient-to-br from-gray-800 to-gray-900 border-4 border-gray-700 shadow-2xl flex items-center justify-center">
-                            <div className="w-20 h-20 rounded-full bg-gray-900 flex items-center justify-center">
-                                <div className="w-12 h-12 rounded-full bg-black border-2 border-gray-600 flex items-center justify-center">
-                                    <div className="w-6 h-6 rounded-full bg-gray-900"></div>
+                    <div className="absolute top-60 left-12 right-12 flex items-center justify-between">
+
+                        <div className="relative">
+                            <div className="w-28 h-28 rounded-full bg-gradient-to-br from-gray-800 to-gray-900 border-4 border-gray-700 shadow-2xl flex items-center justify-center">
+                                <div className="w-20 h-20 rounded-full bg-gray-900 flex items-center justify-center">
+                                    <div className="w-12 h-12 rounded-full bg-black border-2 border-gray-600 flex items-center justify-center">
+                                        <div className="w-6 h-6 rounded-full bg-gray-900"></div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div className="flex-1 mx-6 h-16 relative">
+                            <div className="absolute inset-0 bg-gradient-to-b from-gray-900 to-black rounded-sm border-2 border-gray-800 overflow-hidden">
+                                <div className="absolute inset-0 flex items-center justify-center">
+                                    <div className="w-full h-4 bg-gradient-to-r from-amber-950 via-amber-900 to-amber-950"></div>
                                 </div>
                             </div>
                         </div>
 
+                        <div className="relative">
+                            <div className="w-28 h-28 rounded-full bg-gradient-to-br from-gray-800 to-gray-900 border-4 border-gray-700 shadow-2xl flex items-center justify-center">
+                                <div className="w-20 h-20 rounded-full bg-gray-900 flex items-center justify-center">
+                                    <div className="w-12 h-12 rounded-full bg-black border-2 border-gray-600 flex items-center justify-center">
+                                        <div className="w-6 h-6 rounded-full bg-gray-900"></div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
+
+
+
+                    <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-24 h-4 bg-gray-900 border-t-2 border-gray-700"></div>
+
                 </div>
 
 
+            </div>
 
-                <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-24 h-4 bg-gray-900 border-t-2 border-gray-700"></div>
+
+
+            <div className="experience-tv  relative w-[600px] h-[380px] border-t-8 rounded-r-2xl border-amber-900">
+                <div className="absolute inset-0 bg-gradient-to-b from-gray-900/95 to-black/95 rounded-md shadow-2xl border-2 border-gray-700">
+
+                    <div className="absolute top-4 left-4 right-4 h-28 bg-gradient-to-b from-amber-50 to-amber-500 rounded-sm shadow-md border border-orange-200">
+                        {/* <img
+                        src={backgroundImage}
+                        alt={backgroundImage.slice(0, -4)}
+                        className="w-full h-full object-cover  transition-transform duration-700 group-hover:scale-110"
+                    /> */}
+                        <div className="p-2">
+
+                            <div className="text-3xl font-bold text-gray-800 mb-1">{companyName}</div>
+                            <div className="text-2xl text-gray-600">{location}</div>
+                        </div>
+                    </div>
+
+
+
+                    <div className="absolute top-36 left-4 right-4 h-20 bg-gradient-to-r from-gray-600 via-gray-500 to-gray-600 rounded-sm shadow-inner">
+                        <div className="flex items-center justify-between px-8 h-full">
+
+                            <div className="relative">
+                                <div className="w-12 h-12 rounded-2xl bg-gradient-to-br from-gray-800 to-black border-2 border-gray-700 shadow-xl"></div>
+                                <div className="absolute inset-0 flex items-center justify-center">
+                                    L
+                                </div>
+                            </div>
+
+                            <div className="flex-1 mx-6 h-10 bg-black rounded-sm border border-gray-800 shadow-inner flex items-center justify-center">
+                                <div className="text-green-500 text-lg font-mono tracking-wider opacity-80">{position}</div>
+                            </div>
+
+                            <div className="relative">
+                                <div className="w-12 h-12 rounded-2xl bg-gradient-to-br from-gray-800 to-black border-2 border-gray-700 shadow-xl"></div>
+                                <div className="absolute inset-0 flex items-center justify-center">R
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="absolute bottom-1 left-0 right-0 text-center">
+                            <div className="text-white text-xs font-bold tracking-wide">{date}</div>
+                        </div>
+                    </div>
+
+                    <div className="absolute top-60 left-12 right-12 flex items-center justify-between">
+
+                        <div className="relative">
+                            <div className="w-28 h-28 rounded-full bg-gradient-to-br from-gray-800 to-gray-900 border-4 border-gray-700 shadow-2xl flex items-center justify-center">
+                                <div className="w-20 h-20 rounded-full bg-gray-900 flex items-center justify-center">
+                                    <div className="w-12 h-12 rounded-full bg-black border-2 border-gray-600 flex items-center justify-center">
+                                        <div className="w-6 h-6 rounded-full bg-gray-900"></div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div className="flex-1 mx-6 h-16 relative">
+                            <div className="absolute inset-0 bg-gradient-to-b from-gray-900 to-black rounded-sm border-2 border-gray-800 overflow-hidden">
+                                <div className="absolute inset-0 flex items-center justify-center">
+                                    <div className="w-full h-4 bg-gradient-to-r from-amber-950 via-amber-900 to-amber-950"></div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="relative">
+                            <div className="w-28 h-28 rounded-full bg-gradient-to-br from-gray-800 to-gray-900 border-4 border-gray-700 shadow-2xl flex items-center justify-center">
+                                <div className="w-20 h-20 rounded-full bg-gray-900 flex items-center justify-center">
+                                    <div className="w-12 h-12 rounded-full bg-black border-2 border-gray-600 flex items-center justify-center">
+                                        <div className="w-6 h-6 rounded-full bg-gray-900"></div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+
+
+                    <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-24 h-4 bg-gray-900 border-t-2 border-gray-700"></div>
+
+                </div>
+
 
             </div>
         </div>
 
 
-
     )
 }
 
-export default Card
+export default ExperienceCard
